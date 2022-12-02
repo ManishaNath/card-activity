9641d04c202095576765a89172ebb448cacca89a
VladStarostenko <vlad.starostenko@data-lake.co>
2022-12-02 17:10:59 +0100

    Add checkboxes and Terms to disclaimer

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 
 import { Main } from './pages/Main';
 import { Providers } from './Providers';
+import { TermsAndConditions } from './pages/TermAndConditions';
 
 export const App = () => (
     <Providers>
         <Switch>
             <Route exact path="/" component={Main} />
+            <Route
+                exact
+                path="/terms-and-conditions"
+                component={TermsAndConditions}
+            />
             <Redirect exact from="*" to="/" />
         </Switch>
     </Providers>
